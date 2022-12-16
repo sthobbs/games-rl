2e2bbd463379a21905719d441f85ee1a53b83380
Steve Hobbs <hobbs_steve@hotmail.com>
2022-12-16 00:23:17 -0500

    add random move agent

---
+++ agents/Agent_TicTacToe_Random.py
@@ -0,0 +1,14 @@
+import random
+from agents.Agent_TicTacToe import Agent_TicTacToe
+
+class Agent_TicTacToe_Random(Agent_TicTacToe):
+
+    def __init__(self, agent_idx=None):
+        super().__init__(agent_idx)
+    
+    def play_turn(self, state):
+        """the Agent plays a turn, and returns the new game state, along with the move played"""
+        valid = self.valid_moves(state) # get valid moves
+        move = valid[random.randrange(len(valid))] # get random valid move
+        state, move = self.play_move(state, move) # play move
+        return state, move
