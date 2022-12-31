e7f161694b082ee434ce75a0c23ba8704a22e606
Steve Hobbs <hobbs_steve@hotmail.com>
2022-12-31 01:26:42 -0500

    add tic tac toe training script

---
+++ training_TicTacToe.py
@@ -0,0 +1,74 @@
+from pathlib import Path
+from agents.Agent_TicTacToe_MCTS_NN import Agent_TicTacToe_MCTS_NN
+from utils import plot_performance, evaluate_agent
+import torch
+import torch.nn.functional as F
+import random
+import pickle
+random.seed(20)
+
+output_dir = Path('training_output/TicTacToe')
+
+
+def log_stats(p):
+    X = torch.FloatTensor([[-1, -1, -1, -1, -1, -1, -1, -1, -1, 0]])  # empty board
+    p.logger.info(f'loss, tie, win prob: {F.softmax(p.value(X), dim=1)[0]}')
+    x = F.softmax(p.policy(X), dim=1)[0] 
+    p.logger.info(f'first move probs: {x}')
+    p.logger.info(f'avg corner prob: {(x[0] + x[2] + x[6] + x[8]) / 4}')
+    p.logger.info(f'avg edge prob: {(x[1] + x[3] + x[5] + x[7]) / 4}')
+    p.logger.info(f'center prob: {x[4]}')
+
+def train_and_evaluate():
+
+    # make output directory
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    # initialize agent and opponents list
+    p = Agent_TicTacToe_MCTS_NN(simulations=100)
+    p.setup_logger(output_dir/'log.txt')
+    ops = [p.deepcopy_without_data()]
+
+    # initial model evaluation
+    performances = []
+    per = evaluate_agent(p)
+    performances.append(per)
+    log_stats(p)
+
+    # train agent
+    for i in range(15):
+        p.logger.info(f"Iteration {i+1} of 5")
+        
+        # play vs opponent agents and generate data
+        p.gen_data_diff_ops(n=1000, ops=ops, datapoints_per_game=3)
+        
+        # train value and policy networks
+        p.fit(test_size=0.1, refit_datapoints=200000, early_stopping=50,
+            num_epochs=300, verbose=10)
+        
+        # append to opponents list
+        ops.append(p.deepcopy_without_data())
+        
+        # evaluate against random move agent and mcts agent
+        per = evaluate_agent(p)
+        performances.append(per)
+        log_stats(p)
+
+    # evaluate vs random move agent and mcts agent with tau=inf
+    p.logger.info('Evaluate agent with tau=inf')
+    tau = p.tau
+    p.tau = None
+    evaluate_agent(p)
+    p.tau = tau
+
+    # plot performance
+    plot_performance(performances, output_dir)
+
+    # save agents
+    p.to_pickle(f"{output_dir}/agent.pkl")
+    with open(f"{output_dir}/agent_versions.pkl", 'wb') as f:
+        pickle.dump(ops, f)
+
+
+if __name__ == "__main__":
+    train_and_evaluate()
